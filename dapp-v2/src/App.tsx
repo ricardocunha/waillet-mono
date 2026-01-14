374c70579e7a3f49607d9838f48cfeedec3f09c4
Ricardo Cunha <ricardocunha@gmail.com>
2026-01-13 22:24:14 -0300

    Adding the basic version of the Dapp

---
+++ dapp-v2/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-slate-50">
+      <h1 className="text-2xl font-bold p-8">wAIllet DApp</h1>
+      <p className="px-8 text-slate-400">Under construction...</p>
+    </div>
+  )
+}
+
+export default App
