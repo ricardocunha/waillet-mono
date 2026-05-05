1d468ada4618c3c2448bc038d14a905d9ff5e992
Ricardo Cunha <ricardocunha@gmail.com>
2026-05-05 10:36:27 -0300

    adding base code for Dapp

--- dapp/src/main.tsx
+++ dapp/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { initConfig } from './services/configLoader'
+import { ToastProvider } from './components/Toast'
 
 initConfig().then(() => {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </StrictMode>,
   )
 })
